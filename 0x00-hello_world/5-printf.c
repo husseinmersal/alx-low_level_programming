eeabd16aeae4bd720ea5a1338c800ddddce95052
husseinmersal <husseinmersal740@gmail.com>
2023-08-10 07:05:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ * new description
+ * another description
+ * last description
+ */
+int main(void)
+{
+	/* comment */
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
