e4f3cb66724d50eeb174f92ed5c9071df111b1d9
husseinmersal <husseinmersal740@gmail.com>
2023-08-15 04:22:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_aphabet -make alphabet
+ *
+ * Return 0 always
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+		_putchar('\n');
+	}
+}
