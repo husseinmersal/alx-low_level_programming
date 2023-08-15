4af9a864276e88eb1fca06be7d331b79ecfbeda4
husseinmersal <husseinmersal740@gmail.com>
2023-08-15 04:34:24 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10()
 {
 	char ch;
 	int i=0;
-	for(i=0; i<=9; i++)
+	for(i = 0; i <= 9; i++)
 	{
-		for(ch='a'; ch<='z'; ch++)
+		for(ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
