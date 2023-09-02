e6f75851ac24eafb31f4cee30a7ecf2a81ce4669
husseinmersal <husseinmersal740@gmail.com>
2023-09-02 05:29:12 -0700

    last

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,18 +11,14 @@ int main(int argc , char *argv[])
 {
 	int sum = 0;
 	char *c;
-	while(--argc)
+	while (--argc)
 	{
 		for(c = argv[argc]; *c; c++)
-		{
-		
 			if(*c < '0' || *c > '9')
-			
 				return (printf("error\n"), 1);
-			sum += atoi(argv[argc]);
-		}
+		sum += atoi(argv[argc]);
 	}
-	printf("%d",sum);
+	printf("%d\n",sum);
 	return 0;
 }
        
