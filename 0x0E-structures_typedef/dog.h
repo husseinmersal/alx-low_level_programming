aa56990d492c7ad2bcad8f930f4b8175f069f2e7
husseinmersal <husseinmersal740@gmail.com>
2023-09-11 06:02:50 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
-/**
- * struct dog
- * @name - dog's name 
- * @age - dog's age
- * @owner - dog's owner name
- */
 
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-#endif 
+
+
+#endif
+
+
